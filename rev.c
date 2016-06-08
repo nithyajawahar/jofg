49b65a4e9db8c585c964aaa7c0f7990d70754db9
nithyajawahar <nithyaj.cs13@bitsathy.ac.in>
2016-06-08 09:04:11 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+char a;
+int i,j;
+printf("\nEnter the string");
+scanf("%s",a);
+i=0;
+j=strlen(a)-1;
+while(i<j)
+{
+a[i]=temp;
+a[i]=a[j];
+a[j]=temp;
+i++;
+j--;
+}
+printf("\nThe reversed string is",%s);
+return 0;
+}
